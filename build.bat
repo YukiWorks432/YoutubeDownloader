5c30d17db12330f48bb402fbc8d2230fa218c64d
yuki <nasubich893@gmail.com>
2020-01-17 02:48:04 +0900

    packaging

--- build.bat
+++ build.bat
@@ -1,6 +1,24 @@
-rm ../YoutubeDownloader.exe
+set app=YoutubeDownloader
+set ver=0.0.1
 cd build
-rem rm -rf *
-cmake -G "MSYS Makefiles" ..
-make
+::rm -rf *
+::cmake -G "MSYS Makefiles" ..
+::make
+call :ZIP
+
+:RET
 cd ..
+exit /b
+
+:ZIP
+cd ../../
+mkdir %app%
+cd ./%app%
+rsync -a ../ ./ --exclude "/%app%/"
+Python ./copydll.py ./%app%.exe
+rm ./copydll.py
+cd ../
+PowerShell Compress-Archive -Path ./%app% -DestinationPath ./%app%_v%ver%.zip -Force
+rm -r -f %app%
+cd ./%app%-src
+exit /b
